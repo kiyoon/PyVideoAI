70ef9a7b0a2da85d7d9b374b16e1b6e08683d763
Kiyoon Kim <yoonkr33@gmail.com>
2021-07-10 15:38:46 +0900

    cameramotion experiments

---
+++ exp_configs/ch_tcswap/cater_task2_cameramotion/trn_resnet50-TCswap_33frame.py
@@ -0,0 +1,14 @@
+import os
+_SCRIPT_DIR = os.path.dirname(os.path.abspath( __file__ ))
+exec(open(f'{_SCRIPT_DIR}/../sparsesample_TCswap_crop224_8frame_largejit_plateau_10scrop.py').read())
+
+input_frame_length = 33
+def batch_size():
+    '''batch_size can be either integer or function returning integer.
+    '''
+    vram = torch.cuda.get_device_properties(0).total_memory
+    if vram > 20e+9:
+        return 12
+    elif vram > 10e+9:
+        return 6
+    return 3
