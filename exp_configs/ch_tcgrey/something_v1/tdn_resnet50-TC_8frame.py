1275dd132ddf799afdc62fbcac05a1ecbcb9df2c
Kiyoon Kim <yoonkr33@gmail.com>
2021-08-14 23:05:35 +0900

    fix TDN TC

--- exp_configs/ch_tcgrey/something_v1/tdn_resnet50-TC_8frame.py
+++ exp_configs/ch_tcgrey/something_v1/tdn_resnet50-TC_8frame.py
@@ -4,3 +4,5 @@ exec(open(f'{_SCRIPT_DIR}/../sparsesample_TC_crop224_8frame_largejit_plateau_10s
 
 sample_index_code = 'TDN'
 clip_grad_max_norm = 20
+
+sort_idx = get_TC_sort_idx(input_frame_length*5)   # when you change input frame number, the ordering has to be recalculated.
