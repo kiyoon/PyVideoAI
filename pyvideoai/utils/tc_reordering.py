e86a2426f6a37451480b8e47327f0aadaff8dc94
Kiyoon Kim <yoonkr33@gmail.com>
2021-08-24 23:17:10 +0900

    TC short-long added

--- pyvideoai/utils/tc_reordering.py
+++ pyvideoai/utils/tc_reordering.py
@@ -1,3 +1,35 @@
+def get_frame_idx_1D(frame_idx, colour='R'):
+    """
+    frame_idx (int): starts from one
+
+    example:
+        get_frame_idx_1D(1, 'R') == 0
+        get_frame_idx_1D(2, 'R') == 3
+        get_frame_idx_1D(2, 'G') == 4
+    """
+    assert colour in ['R', 'G', 'B']
+    assert frame_idx > 0
+    # first calculate index for R
+    R_idx = (frame_idx-1) * 3
+    if colour == 'R':
+        return R_idx
+    elif colour == 'G':
+        return R_idx + 1
+    elif colour == 'B':
+        return R_idx + 2
+
+def get_frame_indices_1D(list_frame_colour):
+    """
+    example:
+        get_frame_indices_1D(['1R', '2R', '3R']) == [0, 3, 6]
+    """
+    def get_frame_idx_from_str(str_frame_colour):
+        return get_frame_idx_1D(int(str_frame_colour[:-1]), str_frame_colour[-1])
+
+    return list(map(get_frame_idx_from_str, list_frame_colour))
+
+
+
 def TC_idx(T):
     """
     Returns TC Reordering indices.
@@ -5,7 +37,8 @@ def TC_idx(T):
     Returns indices to make it (1R 2R 3R 2G 3G 4G 3B 4B 5B 4R 5R 6R ...).
     """
     assert T >= 3
-    idx_3frame = [0, 3, 6, 4, 7, 10, 8, 11, 14] # (1R 2R 3R) (2G 3G 4G) (3B 4B 5B)
+    #idx_3frame = [0, 3, 6, 4, 7, 10, 8, 11, 14] # (1R 2R 3R) (2G 3G 4G) (3B 4B 5B)
+    idx_3frame = get_frame_indices_1D(['1R', '2R', '3R', '2G', '3G', '4G', '3B', '4B', '5B'])
     repeat = T//3 + (T%3>0) #math.ceil(T / 3)
     idx = []
     for i in range(repeat):
@@ -18,6 +51,7 @@ def TC_idx(T):
     idx = list(map(frame_limit, idx))
     return idx
 
+
 def TCred_idx(T):
     """
     Returns TC Reordering indices, but using only red channel.
@@ -25,7 +59,7 @@ def TCred_idx(T):
     Returns indices to make it (1R 2R 3R 2R 3R 4R 3R 4R 5R 4R 5R 6R ...).
     """
     assert T >= 3
-    idx_1frame = [0, 3, 6] # (1R 2R 3R)
+    idx_3frame = get_frame_indices_1D(['1R', '2R', '3R'])
     repeat = T
     idx = []
     for i in range(repeat):
@@ -35,6 +69,19 @@ def TCred_idx(T):
             x -= 3
         return x
     idx = list(map(frame_limit, idx))
+    return idx
+
+def TCShortLong_idx(T):
+    """
+    TC Short-Long
+    Returns TC Reordering indices, but including long-term as well as short-term sampling.
+    Idea is instead of duplicating channels, use long-term sampling.
+    When channels are ordered like (1R 1G 1B 2R 2G 2B 3R 3G 3B 4R ...),
+    Returns indices to make it (1R 2R 3R 2G 3G 4G 3B 4B 5B 4R 5R 6R 5G 6G 7G 6B 7B 8B '3R 5R 7R 4G 6G 8G'...).
+    """
+    assert T == 8, 'only supporting 8-frame'
+    idx = TC_idx(T)
+    idx[-6:] = get_frame_indices_1D(['3R', '5R', '7R', '4G', '6G', '8G'])
     return idx
 
 TC_idx_dict = {}
