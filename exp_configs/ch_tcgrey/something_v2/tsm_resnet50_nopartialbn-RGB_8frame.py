44814046f5112f68ec6286f95b573567e768112c
Kiyoon Kim <yoonkr33@gmail.com>
2021-08-28 06:55:43 -0400

    tsm sthv2 train longer

--- exp_configs/ch_tcgrey/something_v2/tsm_resnet50_nopartialbn-RGB_8frame.py
+++ exp_configs/ch_tcgrey/something_v2/tsm_resnet50_nopartialbn-RGB_8frame.py
@@ -8,4 +8,6 @@ base_learning_rate = 0.01 / 64     # when batch_size == 1 and #GPUs == 1
 
 
 def scheduler(optimiser, iters_per_epoch, last_epoch=-1):
-    return torch.optim.lr_scheduler.MultiStepLR(optimiser, [it*iters_per_epoch for it in [20, 40]], 0.1, last_epoch)
+    return torch.optim.lr_scheduler.MultiStepLR(optimiser, [it*iters_per_epoch for it in [20, 40, 50, 60, 70, 80, 90, 100]], 0.1, last_epoch)
+
+load_scheduler_state = False
