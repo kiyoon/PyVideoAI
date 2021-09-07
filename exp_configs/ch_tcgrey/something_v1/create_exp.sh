f184b6495287e3170047ee1c0f00c8fe6e96a95d
Kiyoon Kim <yoonkr33@gmail.com>
2021-09-08 03:51:50 +0900

    TCred on SthV1

--- exp_configs/ch_tcgrey/something_v1/create_exp.sh
+++ exp_configs/ch_tcgrey/something_v1/create_exp.sh
@@ -1,6 +1,6 @@
 for model in tsn_resnet50 trn_resnet50 mtrn_resnet50 
 do
-	for sampling_mode in RGB TC TCrgb GreyST
+	for sampling_mode in RGB TC TCrgb TCred GreyST
 	do
 		ln -s ../sparsesample_${sampling_mode}_crop224_8frame_largejit_plateau_10scrop.py $model-${sampling_mode}_8frame.py
 	done
---
+++ exp_configs/ch_tcgrey/something_v1/trn_resnet50-TCred_8frame.py
@@ -0,0 +1,1 @@
+../sparsesample_TCred_crop224_8frame_largejit_plateau_10scrop.py
