cdb823d3e27ff2ab5eea1f1f90a8fea355ff0523
Kiyoon Kim <yoonkr33@gmail.com>
2021-11-25 07:42:31 +0900

    epic55 verb with labelsmooth and proselflc

---
+++ exp_configs/ch_labelsmth/epic55_verb/tsm_resnet50_nopartialbn-proselflc.py
@@ -0,0 +1,7 @@
+_exec_relative_('../sparsesample_onehot_RGB_crop224_8frame_largejit_plateau.py')
+_exec_relative_('../tsm_resnet50_nopartialbn_base.py')
+
+
+label_mode = 'proselflc'
+proselflc_total_time = 362 * 60 # 60 epochs
+proselflc_exp_base = 1.
