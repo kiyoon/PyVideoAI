99647c616dd851220cc44200fc8c8b80d05950ac
Kiyoon Kim <kiyoon.kim@ed.ac.uk>
2022-02-17 17:01:06 +0000

    generate epic100 flow splits (not done)

---
+++ tools/datasets/epic_convert_rgb_to_flow_frame_idxs.py
@@ -0,0 +1,51 @@
+# Code from:
+# https://github.com/epic-kitchens/C1-Action-Recognition-TSN-TRN-TSM/blob/master/src/convert_rgb_to_flow_frame_idxs.py
+
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+    description="Convert the frame ids from RGB frames to flow frame ids.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("labels_in_pkl", type=Path)
+parser.add_argument("labels_out_pkl", type=Path)
+
+
+# For videos from 2018 we extracted flow by a temporal stride of 2, so there are
+# half the number of flow frames as RGB
+# For 2020 we didn't subsample frames, so there is almost a 1-to-1 correspondence
+# (bar the last frame, given optical flow is computed between pairs of frames).
+STRIDES = {
+    2018: 2,
+    2020: 1,
+}
+
+
+def convert_rgb_frame_to_flow_frame_idx(frame_idxs, stride: int):
+    return (frame_idxs - 1) // stride + 1
+
+
+def main(args):
+    df = pd.read_pickle(args.labels_in_pkl)
+    # Videos from 2018 are of the format PXX_YY and from 2020 are PXX_1YY
+    df["year"] = df["video_id"].apply(
+        lambda id_: 2020 if len(id_.split("_")[-1]) == 3 else 2018
+    )
+    columns_to_convert = [c for c in df.columns if c.endswith("frame")]
+    year_masks = {year: df["year"] == year for year in [2020, 2018]}
+
+    for year, year_mask in year_masks.items():
+        stride = STRIDES[year]
+        for col in columns_to_convert:
+            df.loc[year_mask, col] = convert_rgb_frame_to_flow_frame_idx(
+                df.loc[year_mask, col], stride
+            )
+
+    df.to_pickle(args.labels_out_pkl)
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
---
+++ tools/datasets/generate_epic100_flow_splits.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import tqdm
+import pickle
+import os
+from decord import VideoReader
+from video_datasets_api.epic_kitchens_100.read_annotations import epic_narration_id_to_unique_id
+
+
+import argparse
+def get_parser():
+    parser = argparse.ArgumentParser(description="Generate EPIC-Kitchens-100 verb-only flow splits. You MUST run epic_convert_rgb_to_flow_frame_idxs.py before running this.",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("root_55", help="Path to the directory with flow files. (EPIC-55 directory structure)")
+    parser.add_argument("root_100", help="Path to the directory with flow files. (EPIC-100 extensions directory)")
+    parser.add_argument("output_dir", help="Directory to save train.csv and val.csv")
+    parser.add_argument("annotations_root_dir", type=str, help="Path to annotations root dir.")
+    return parser
+
+parser = get_parser()
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    os.makedirs(args.output_dir, exist_ok=True)
+    narration_id_to_uid, _ = epic_narration_id_to_unique_id(args.annotations_root_dir)
+
+    for split in ['train', 'validation']:
+        label_path = os.path.join(args.annotations_root_dir, f'EPIC_100_{split}_flow.pkl')
+        if not os.path.isfile(label_path):
+            raise FileNotFoundError(f'{label_path} not available. Did you forget to run epic_convert_rgb_to_flow_frame_idxs.py?')
+            
+        with open(label_path, 'rb') as f:
+            epic_action_labels = pickle.load(f)
+
+        # output name is train.csv and val.csv
+        if split == 'validation':
+            split = 'val'
+        
+        split_file = open(os.path.join(args.output_dir, f'{split}.csv'), 'w')
+
+        split_file.write('0')
+
+        num_videos = len(epic_action_labels.index)
+        for index in tqdm.tqdm(range(num_videos), desc="Generating splits"):
+            narration_id = epic_action_labels.index[index]
+            uid = narration_id_to_uid[narration_id]
+            verb_label = epic_action_labels.verb_class.iloc[index]
+
+            dir_path = os.path.join(args.root, narration_id)
+            
+            start_frame = epic_action_labels.start_frame.iloc[index]
+            stop_frame = epic_action_labels.stop_frame.iloc[index]
+
+            write_str = f'\n{split}/{narration_id} {uid} {verb_label} {start_frame} {stop_frame}'
+
+            split_file.write(write_str)
+
+        split_file.close()
