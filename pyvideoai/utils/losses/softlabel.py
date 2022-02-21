7c95c46f2cba3ec16c3dfdf6b352f812149a2777
Kiyoon Kim <yoonkr33@gmail.com>
2022-02-22 03:36:52 +0900

    softlabel regression loss

---
+++ pyvideoai/utils/losses/softlabel.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+class SoftlabelRegressionLoss(nn.Module):
+    """Similar to negative log likelihood,
+    but we use sigmoid instead of softmax and we have soft labels
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs, targets):
+        preds = F.sigmoid(inputs)
+
+        loss = (targets * torch.log(preds)) + ((1 - targets) * torch.log(1-preds))
+
+        return -loss.sum(dim=-1))
