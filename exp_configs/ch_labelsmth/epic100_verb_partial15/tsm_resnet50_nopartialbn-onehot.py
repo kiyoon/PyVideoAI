cf935b189c435b60e49c8974daac7145ff1fe456
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-01 20:53:49 +0000

    onehot RGB exp

--- exp_configs/ch_labelsmth/epic100_verb_partial15/tsm_resnet50_nopartialbn-onehot.py
+++
@@ -1,9 +0,0 @@
-_exec_relative_('../sparsesample_onehot_RGB_crop224_8frame_largejit_plateau.py')
-_exec_relative_('../tsm_resnet50_nopartialbn_base.py')
-
-base_learning_rate = 5e-6
-
-
-pretrained_path = '/home/kiyoon/fast/experiments_labelsmooth/epic55_verb/tsm_resnet50_nopartialbn/onehot/version_002/weights/best.pth'
-def load_pretrained(model):
-    loader.model_load_weights_GPU_partial_nostrict(model, pretrained_path)
