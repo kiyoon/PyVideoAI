cf2d5ec3ab32e64e765820a296c07adbdd580992
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-04 17:02:16 +0000

    topk multilabel accuracy

--- pyvideoai/metrics/top1_multilabel_accuracy.py
+++ pyvideoai/metrics/top1_multilabel_accuracy.py
@@ -113,3 +113,119 @@ class ClipTop1MultilabelAccuracyMetric(Metric):
     def __len__(self):
         return self.num_seen_samples
 
+
+class ClipTopkMultilabelAccuracyMetric(Metric):
+    """
+    Count as true positive when the top-k prediction is in one of the k multiple ground truth labels.
+
+    Don't need activation softmax for clip accuracy calculation.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def clean_data(self):
+        super().clean_data()
+        self.num_seen_samples = 0
+        self.num_true_positives = 0
+
+
+    def add_clip_predictions(self, video_ids, clip_predictions, labels):
+        video_ids, clip_predictions, labels = super().add_clip_predictions(video_ids, clip_predictions, labels)
+        if video_ids is None:
+            return      # sometimes, after filtering the samples, there can be no samples to do anything.
+
+        assert labels.dim() == 2, f'labels has to be a 2D tensor with ones and zeros but got {labels.dim()}-D.'
+
+        for pred, label in zip(clip_predictions, labels):
+            labels_cur_sample = torch.where(labels==1.)[0]
+            num_labels = label_cur_sample.size(0)
+            num_nonlabels = torch.where(labels==0.)[0].size(0)
+            if num_labels + num_nonlabels != self.num_classes:
+                raise ValueError(f'Label in Top-K Multilabel Accuracy metric has to be ones and zeros but got something else.'
+            
+            self.num_seen_samples += 1
+            _, top_classes = torch.topk(pred, num_labels)
+            for top_class in top_classes:
+                if top_class in labels_cur_sample:
+                    self.num_true_positives += 1
+                    break
+
+
+    def calculate_metrics(self):
+        self.last_calculated_metrics = self.num_true_positives / self.num_seen_samples if self.num_seen_samples > 0 else 0.
+
+    def types_of_metrics(self):
+        return float
+
+
+    def tensorboard_tags(self):
+        return 'Top-K multilabel accuracy'
+
+
+    def get_csv_fieldnames(self):
+        return f'{self.split}_topk_multilabel_accuracy'
+
+
+    def logging_msg_iter(self):
+        """
+        Return:
+            None to skip logging this metric
+            or a single str that combines all self.last_calculated_metrics 
+        """
+        if self.split == 'train':
+            prefix = ''
+        else:
+            prefix = f'{self.split}_'
+
+        message = f'{prefix}topkmultiacc: {self.last_calculated_metrics:.4f}'
+        return message
+
+
+    def logging_msg_epoch(self):
+        """
+        Return:
+            None to skip logging this metric
+            or a single str that combines all self.last_calculated_metrics 
+        """
+        return self.logging_msg_iter()
+
+
+    def plot_legend_labels(self):
+        """
+        Return:
+            either tuple or a single str 
+        """
+        if self.split == 'train':
+            return f'Training top-k multilabel accuracy'
+        elif self.split == 'val':
+            return f'Validation top-k multilabel accuracy'
+        elif self.split == 'multicropval':
+            return f'Multicrop validation top-k multilabel accuracy'
+        else:
+            return f'{self.split} top-k multilabel accuracy'
+
+
+    def plot_file_basenames(self):
+        """
+        Return:
+            either tuple or a single str 
+        """
+        # output plot file names will be e.g.) accuracy.png/pdf, accuracy_top5.png/pdf, ...
+        return 'topk_multilabel_accuracy'
+
+    
+    @staticmethod
+    def is_better(value_1, value_2):
+        """Metric comparison function
+
+        params:
+            two values of the metric
+
+        return:
+            True if value_1 is better. False if value_2 is better or they're equal.
+        """
+        return value_1 > value_2 
+
+    def __len__(self):
+        return self.num_seen_samples
+
