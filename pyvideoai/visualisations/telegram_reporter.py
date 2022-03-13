1fa7be38ad76036ba27a7abbd29d0959e459cf66
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-13 11:15:57 +0000

    telegram reports W&B URLs

--- pyvideoai/visualisations/telegram_reporter.py
+++ pyvideoai/visualisations/telegram_reporter.py
@@ -7,7 +7,8 @@ import torch
 from .. import __version__
 
 class DefaultTelegramReporter:
-    def __init__(self, include_version: bool = True, include_exp_rootdir: bool = False, ignore_figures: bool = False):
+    def __init__(self, include_wandb_url: bool = True, include_version: bool = True, include_exp_rootdir: bool = False, ignore_figures: bool = False):
+        self.include_wandb_url = include_wandb_url
         self.include_version = include_version
         self.include_exp_rootdir = include_exp_rootdir
         self.ignore_figures = ignore_figures
@@ -15,8 +16,16 @@ class DefaultTelegramReporter:
     def report(self, metrics: Metrics, exp: ExperimentBuilder, figs) -> None:
         #telegram_report_msgs = [f'Running on {socket.gethostname()}, Plots at epoch {exp.summary["epoch"].iloc[-1]:d}']
         telegram_report_msgs = []
+        if self.include_wandb_url:
+            try: 
+                import wandb
+                if wandb.run is not None:
+                    telegram_report_msgs.append(f'W&B Project: {wandb.run.get_project_url()}')
+                    telegram_report_msgs.append(f'W&B Run: {wandb.run.get_url()}')
+            except ImportError:
+                pass
         if self.include_version:
-            telegram_report_msgs.append(f'PyTorch=={torch.__version__}, PyVideoAI=={__version__}')
+            telegram_report_msgs.append(f'PyTorch={torch.__version__}, PyVideoAI={__version__}')
         if self.include_exp_rootdir:
             telegram_report_msgs.append(f'Experiment root: {exp.experiment_root}')
         
