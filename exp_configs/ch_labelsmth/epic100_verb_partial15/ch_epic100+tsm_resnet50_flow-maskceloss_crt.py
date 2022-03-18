52d180d05bf7b446016c776a304b3a81f8cb550d
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-19 01:12:51 +0900

    cRT, imagenet pre experiments

--- exp_configs/ch_labelsmth/epic100_verb_partial15/ch_epic100+tsm_resnet50_flow-maskceloss_crt.py
+++ exp_configs/ch_labelsmth/epic100_verb_partial15/ch_epic100+tsm_resnet50_flow-maskceloss_crt.py
@@ -1,6 +1,7 @@
-_exec_relative_('../sparsesample_onehot_crop224_8frame_largejit_plateau.py')
+_exec_relative_('../neighbour_multilabel.py')
 
 input_type = 'gulp_flow'
+loss_type = 'maskce'
 num_epochs = int(os.getenv('VAI_NUM_EPOCHS', 50))
 train_classifier_balanced_retraining_epochs = int(os.getenv('VAI_CRT_EPOCHS', 15))    # How many epochs to re-train the classifier from random weights, in a class-balanced way, at the end. Bingyi Kang et al. 2020, (cRT)
 early_stop_patience = None
